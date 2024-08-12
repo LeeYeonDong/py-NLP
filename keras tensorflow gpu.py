ee84646518ba3e9c341f5f662aa2fb093b53eef1
LeeYeonDong <LeeYeonDong@users.noreply.github.com>
2024-08-13 02:59:56 +0900

    Add files via upload

--- keras tensorflow gpu.py
+++ keras tensorflow gpu.py
@@ -1,11 +1,12 @@
 10/29
 
-import os
+import sys, os
 print(os.getcwd())
 
 import numpy as np
+print(np.__version__)
 
-# python -m pip install --user tensorflow-gpu==2.9.1  
+# python -m pip install --user tensorflow-gpu 
 import tensorflow as tf
 print(tf.test.is_built_with_cuda())
 print(tf.config.list_physical_devices('GPU'))
@@ -14,9 +15,7 @@ tf.__version__
 from tensorflow import keras
 keras.__version__
 
-tf.test.is_gpu_available()
-
-import tensorflow
+import tensorflow as tf
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
--- tm-bertopic.py
+++ tm-bertopic.py
@@ -4,11 +4,13 @@ import numpy as np
 import pandas as pd
 from bertopic import BERTopic
 
+
 ## dynamic topic modeling
 import re
 
 # Prepare data
-trump = pd.read_csv("D:/대학원/논문/Topic Modeling/trump.csv", encoding = "utf-8")
+trump = pd.read_csv("D:/대학원/논문/topic Model/trump.csv", encoding = "utf-8")
+sports = pd.read_csv("D:/대학원/논문/소논문/스포츠기사/뉴스_sports.news_야구.csv", encoding = "utf-8")
 
 # preprocessing data
 trump.text = trump.apply(lambda row: re.sub(r"http\S+", "", row.text).lower(), 1)
@@ -24,22 +26,29 @@ trump[0:6]
 # BERTopic
 # 두 가지 출력인 토픽(topics)과 확률(probabilities)이 만들어집니다. 토픽의 값은 단순히 할당된 토픽을 나타냅니다. 반면에 확률은 문서가 일어날 수 있는 토픽에 포함될 가능성
 topic_model = BERTopic(min_topic_size = 70, n_gram_range = (1,3), verbose = True, calculate_probabilities = True)
+
+# Umap
+from umap import UMAP
+
+umap_model = UMAP(n_neighbors=15, n_components=5, min_dist=0.0, metric='cosine')
+
+topic_model = BERTopic(nr_topics = "auto", n_gram_range = (1,3), verbose = True, calculate_probabilities = True, umap_model = umap_model, embedding_model="all-MiniLM-L6-v2")
+
 # or topic_model = BERTopic(nr_topics = "auto")
 topics, probabilities = topic_model.fit_transform(trump["text"].to_list())
+topics, probabilities = topic_model.fit_transform(sports["제목_야구"].to_list())
 
 # corresponding timestamps, and the related topics
 # bins
-# topics_over_time = topic_model.topics_over_time(tweets, timestamps, nr_bins = 20)
+topics_over_time = topic_model.topics_over_time(tweets, timestamps, nr_bins = 20)
 
 # Parameters
 # Tuning
-# topics_over_time = topic_model.topics_over_time(tweets, timestamps, global_tuning=True, evolution_tuning=True, nr_bins=20)
-# Datetime format
-topics_over_time_date = topic_model.topics_over_time(tweets, timestamps, datetime_format="%Y/%m/%d", nr_bins = 20)
+topics_over_time = topic_model.topics_over_time(tweets, timestamps, global_tuning=True, evolution_tuning=True, nr_bins=20)
 
 # Visualization 1
-topic_model.visualize_topics_over_time(topics_over_time_date, top_n_topics = 20).show()
-topic_model.visualize_topics_over_time(topics_over_time_date, topics = [9, 10, 11, 15, 8, 12]).show()
+topic_model.visualize_topics_over_time(topics_over_time, top_n_topics = 20).show()
+topic_model.visualize_topics_over_time(topics_over_timeate, topics = [9, 10, 11, 15, 8, 12]).show()
 
 # Visualization 2 
 # 상대적 빈도(relative frequency)로 생성된 토픽에 액세스
@@ -55,21 +64,18 @@ topic_model.get_topic(topic_nr)
 topic_model.visualize_topics(top_n_topics = 20).show()
 
 # Visualization 3
-topic_model.visualize_topics_over_time(topics_over_time_date, top_n_topics = 10).show()
-
-# Visualization 4
 topic_model.visualize_barchart(top_n_topics = 20).show()
 
-# Visualization 5
+# Visualization 4
 topic_model.visualize_term_rank().show()
 
-# Visualization 6
+# Visualization 5
 topic_model.visualize_hierarchy(top_n_topics = 20).show()
 
-# Visualization 7
+# Visualization 6
 topic_model.visualize_heatmap(top_n_topics = 20).show()
 
-# Visualization 8
+# Visualization 7
 topic_model.estimate()
 topic_model.visualize_distribution(topic_model.probabilities_[0], min_probability = 0.015).show()
 topic_model.visualize_distribution(probabilities[0]).show()
